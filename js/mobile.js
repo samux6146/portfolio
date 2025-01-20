5706af91b09726b8a9c267705ef8b23d42ea1e4d
Samuel Fischer <mail@samuelfischer.it>
2025-01-20 21:49:18 +0100

    added mobile support

---
+++ js/mobile.js
@@ -0,0 +1,4 @@
+document.getElementById('command').addEventListener('click', function(){
+    var inputElement = document.getElementById('hiddenInput');
+    inputElement.select(); // focus on it so keyboard pops
+});
