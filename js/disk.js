f416b8d296d87a1e906c073632a52a3eacbf24a4
Samuel Fischer <mail@samuelfischer.it>
2025-02-26 17:42:23 +0100

    improved readability of disk data

--- js/disk.js
+++ js/disk.js
@@ -10,22 +10,47 @@ const disk = {
                 "cw2usb": {
                     "name": "cw2usb",
                     "type": "file",
-                    "content": '<hr class="line"> <h1 style="color: white;">Cw2Usb</h1> <h3 style="color: white;">A small adaptor based on an RP2040-Zero microcontroller board, to connect a cw (morse) key to a computer. Supports both paddles and straight keys without any configuration!</h3> <a class="gitlink" href="https://github.com/samux6146/Cw2Usb" target="_blank">[ Go to GitHub --> ]</a> <hr class="line">'
+                    "content": '<hr class="line">   \
+                                <h1 style="color: white;">Cw2Usb</h1>   \
+                                <h3 style="color: white;" class="text_disk">  \
+                                    A small adaptor based on an RP2040-Zero microcontroller board, to connect a cw (morse) key to a computer.   \
+                                    Supports both paddles and straight keys without any configuration!  \
+                                </h3>   \
+                                <a class="gitlink" href="https://github.com/samux6146/Cw2Usb" target="_blank">[ Go to GitHub --> ]</a>  \
+                                <hr class="line">'
                 },
                 "croxsplit44": {
                     "name": "croxsplit44",
                     "type": "file",
-                    "content": '<hr class="line"> <h1 style="color: white;">Croxsplit44</h1> <h3 style="color: white;">A 3d printed 44 key handwierd split keyboard using Alps / matias switches</h3> <a class="gitlink" href="https://github.com/samux6146/croxsplit44" target="_blank">[ Go to GitHub --> ]</a> <hr class="line">'
+                    "content": '<hr class="line">   \
+                                <h1 style="color: white;">Croxsplit44</h1>  \
+                                <h3 style="color: white;" class="text_disk">  \
+                                    A 3d printed 44 key handwierd split keyboard using Alps / matias switches   \
+                                </h3>   \
+                                <a class="gitlink" href="https://github.com/samux6146/croxsplit44" target="_blank">[ Go to GitHub --> ]</a> \
+                                <hr class="line">'
                 },
                 "macconfig": {
                     "name": "macconfig",
                     "type": "file",
-                    "content": '<hr class="line"> <h1 style="color: white;">MacConfig</h1> <h3 style="color: white;">My macos Shell and Terminal config</h3> <a class="gitlink" href="https://github.com/samux6146/MacConfig" target="_blank">[ Go to GitHub --> ]</a> <hr class="line">'
+                    "content": '<hr class="line">   \
+                                <h1 style="color: white;">MacConfig</h1>    \
+                                <h3 style="color: white;" class="text_disk">  \
+                                    My macos Shell and Terminal config  \
+                                </h3>   \
+                                <a class="gitlink" href="https://github.com/samux6146/MacConfig" target="_blank">[ Go to GitHub --> ]</a>   \
+                                <hr class="line">'
                 },
                 "portfolio": {
                     "name": "portfolio",
                     "type": "file",
-                    "content": '<hr class="line"> <h1 style="color: white">Portfolio</h1> <h3 style="color: white;">My portfolio website emulating a unix shell (this website)</h3> <a class="gitlink" href="https://github.com/samux6146/portfolio" target="_blank">[ Go to GitHub --> ]</a> <hr class="line">'
+                    "content": '<hr class="line">   \
+                                <h1 style="color: white">Portfolio</h1> \
+                                <h3 style="color: white;" class="text_disk">  \
+                                    My portfolio website emulating a unix shell (this website)  \
+                                </h3>   \
+                                <a class="gitlink" href="https://github.com/samux6146/portfolio" target="_blank">[ Go to GitHub --> ]</a>   \
+                                <hr class="line">'
                 }
             }
         },
@@ -36,22 +61,46 @@ const disk = {
                 "whatismittelab": {
                     "name": "whatismittelab",
                     "type": "file",
-                    "content": '<hr class="line"> <h1 style="color: white;">What is mittelab?</h1> <h3 style="color: white;">Mittelab is my city&apos;s local Hackerspace</h3> <a class="gitlink" href="https://www.mittelab.org" target="_blank">[ Go to the Mittelab website --> ]</a> <hr class="line">'
+                    "content": '<hr class="line">   \
+                                <h1 style="color: white;">What is mittelab?</h1>    \
+                                <h3 style="color: white;" class="text_disk">  \
+                                    Mittelab is my city&apos;s local Hackerspace    \
+                                </h3>   \
+                                <a class="gitlink" href="https://www.mittelab.org" target="_blank">[ Go to the Mittelab website --> ]</a>   \
+                                <hr class="line">'
                 },
                 "keypong": {
                     "name": "keypong",
                     "type": "file",
-                    "content": '<hr class="line"> <h1 style="color: white;">KeyPong</h1> <h3 style="color: white;">A telegram bot for fast management of our sites opening personnel.</h3> <a class="gitlink" href="https://git.mittelab.org/samu/keypong" target="_blank">[ Go to GitLab --> ]</a> <hr class="line">'
+                    "content": '<hr class="line">   \
+                                <h1 style="color: white;">KeyPong</h1>  \
+                                <h3 style="color: white;" class="text_disk">  \
+                                    A telegram bot for fast management of our sites opening personnel.  \
+                                </h3>   \
+                                <a class="gitlink" href="https://git.mittelab.org/samu/keypong" target="_blank">[ Go to GitLab --> ]</a>    \
+                                <hr class="line">'
                 },
                 "questboard": {
                     "name": "questboard",
                     "type": "file",
-                    "content": '<hr class="line"> <h1 style="color: white;">MittelabQuestBoard</h1> <h3 style="color: white;">A leaderboard of the top quest slayers of mittelab!</h3> <a class="gitlink" href="https://git.mittelab.org/samu/mittelabquestboard" target="_blank">[ Go to GitLab --> ]</a> <hr class="line">'
+                    "content": '<hr class="line">   \
+                                <h1 style="color: white;">MittelabQuestBoard</h1>   \
+                                <h3 style="color: white;" class="text_disk">  \
+                                    A leaderboard of the top quest slayers of mittelab! \
+                                </h3>   \
+                                <a class="gitlink" href="https://git.mittelab.org/samu/mittelabquestboard" target="_blank">[ Go to GitLab --> ]</a> \
+                                <hr class="line">'
                 },
                 "mittelauncher": {
                     "name": "mittelauncher",
                     "type": "file",
-                    "content": '<hr class="line"> <h1 style="color: white">Mittelauncher</h1> <h3 style="color: white;">A Minecraft launcher made for the Mitttelab&apos;s minecraft server!</h3> <a class="gitlink" href="https://git.mittelab.org/samu/mittelauncher" target="_blank">[ Go to GitLab --> ]</a> <hr class="line">'
+                    "content": '<hr class="line">   \
+                                <h1 style="color: white">Mittelauncher</h1> \
+                                <h3 style="color: white;" class="text_disk">  \
+                                    A Minecraft launcher made for the Mitttelab&apos;s minecraft server!    \
+                                </h3>   \
+                                <a class="gitlink" href="https://git.mittelab.org/samu/mittelauncher" target="_blank">[ Go to GitLab --> ]</a>  \
+                                <hr class="line">'
                 }
             }
         }
@@ -60,6 +109,21 @@ const disk = {
 "about_me": {
     "name": "about_me",
     "type": "file",
-    "content": '<hr class="line"> <div style="color: white;"> <h1>About me</h1> <p class="text_disk_bottom text_disk">Hi, I&apos;m Samuel Fischer, a passionate developer from Italy who thrives on solving complex problems and creating innovative solutions. Ever since I started coding, I&apos;ve been fascinated by the intersection of hardware and software, always looking for ways to blend the two seamlessly.</p> <p class="text_disk_bottom text_disk">I believe that "My life is an application, and I am the Developer". This mindset drives me to continuously improve, learn new technologies, and build projects that push the boundaries of what&apos;s possible. Whether it&apos;s designing embedded systems, experimenting with open-source software, or developing custom hardware solutions, I enjoy the challenge of turning ideas into reality.</p> <p class="text_disk">When I&apos;m not coding, you&apos;ll probably find me exploring new tech trends, tinkering with electronics, or diving into creative side projects. I&apos;m always open to collaborations and new opportunities, so feel free to reach out!</p> </div> <hr class="line">'
+    "content": '<hr class="line">   \
+                <div style="color: white;"> <h1>About me</h1>   \
+                    <p class="text_disk_bottom text_disk">  \
+                        Hi, I&apos;m Samuel Fischer, a passionate developer from Italy who thrives on solving complex problems and creating innovative solutions.   \
+                        Ever since I started coding, I&apos;ve been fascinated by the intersection of hardware and software, always looking for ways to blend the two seamlessly.   \
+                    </p>    \
+                    <p class="text_disk_bottom text_disk">I believe that "My life is an application, and I am the Developer".   \
+                        This mindset drives me to continuously improve, learn new technologies, and build projects that push the boundaries of what&apos;s possible.    \
+                        Whether it&apos;s designing embedded systems, experimenting with open-source software, or developing custom hardware solutions, I enjoy the challenge of turning ideas into reality.    \
+                    </p>    \
+                    <p class="text_disk">   \
+                        When I&apos;m not coding, you&apos;ll probably find me exploring new tech trends, tinkering with electronics, or diving into creative side projects.    \
+                        I&apos;m always open to collaborations and new opportunities, so feel free to reach out!    \
+                    </p>    \
+                </div>  \
+                <hr class="line">'
 }
 }
